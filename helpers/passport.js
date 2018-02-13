3782669d76c45902deb4565545fd37fae448b23c
Gonzalo Fornies Cerrada <gonzalo.fornies@gmail.com>
2018-02-13 21:20:13 +0100

    auth con login y signup user y guarderia

--- helpers/passport.js
+++ helpers/passport.js
@@ -1,8 +1,9 @@
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-
+const FbStrategy = require('passport-facebook').Strategy;
 const User = require('../models/user');
+const Guarderia = require('../models/guarderia');
 
 function configurePassport() {
   passport.serializeUser((user, cb) => {
@@ -16,10 +17,33 @@ function configurePassport() {
     });
   });
 
-  passport.use(new LocalStrategy({
+  const myLocalStrategy = new LocalStrategy({
     passReqToCallback: true,
   }, (req, username, password, next) => {
     User.findOne({ username }, (err, user) => {
+      if (err) {
+        console.log('entrando en iferror');
+        return next(err);
+      }
+      if (!user) {
+        console.log('entrando en ifuser');
+        return next(null, false, { message: 'Incorrect username' });
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        console.log('entrando en bcrypt');
+        return next(null, false, { message: 'Incorrect password' });
+      }
+
+      return next(null, user);
+    });
+  });
+  passport.use('local', myLocalStrategy);
+
+
+  const myLocalStrategy2 = new LocalStrategy({
+    passReqToCallback: true,
+  }, (req, username, password, next) => {
+    Guarderia.findOne({ username }, (err, user) => {
       if (err) {
         return next(err);
       }
@@ -32,7 +56,35 @@ function configurePassport() {
 
       return next(null, user);
     });
-  }));
+  });
+  passport.use('local2', myLocalStrategy2);
 }
+
+
+passport.use(new FbStrategy({
+  clientID: '443951352690510',
+  clientSecret: '2745628e88cfd1b32490ab7ea80afa00',
+  callbackURL: '/auth/facebook/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  User.findOne({ facebookID: profile.id }, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+    if (user) {
+      return done(null, user);
+    }
+
+    const newUser = new User({
+      facebookID: profile.id,
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        return done(err);
+      }
+      done(null, newUser);
+    });
+  });
+}));
 
 module.exports = configurePassport;
--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const ensureLogin = require('connect-ensure-login');
 const isLoggedIn = require('../middlewares/auth');
 
 const User = require('../models/user');
+const Guarderia = require('../models/guarderia');
 
 // Bcrypt to encrypt passwords
 const bcrypt = require('bcrypt');
@@ -14,7 +15,7 @@ router.get('/signup', (req, res, next) => {
   res.render('auth/signup');
 });
 
-router.post('/signup', (req, res, next) => {
+router.post('/user/signup', (req, res, next) => {
   const { username } = req.body;
   const { password } = req.body;
   if (username === '' || password === '') {
@@ -42,20 +43,51 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
+router.post('/guarderia/signup', (req, res, next) => {
+  const { username } = req.body;
+  const { password } = req.body;
+  if (username === '' || password === '') {
+    res.render('auth/signup', { message: 'Indicate username and password' });
+    return;
+  }
+  Guarderia.findOne({ username }, 'username', (err, guarderia) => {
+    if (guarderia !== null) {
+      res.render('auth/signup', { message: 'The guarderia already exists' });
+      return;
+    }
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+    const newGuarderia = new Guarderia({
+      username,
+      password: hashPass,
+    });
+    newGuarderia.save((err) => {
+      if (err) {
+        res.render('auth/signup', { message: 'Something went wrong' });
+      } else {
+        res.redirect('/');
+      }
+    });
+  });
+});
+
 router.get('/login', (req, res, next) => {
   res.render('auth/login', { message: req.flash('error') });
 });
 
-router.post('/login', passport.authenticate('local', {
+router.post('/user/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login',
   failureFlash: true,
   passReqToCallback: true,
 }));
 
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('private', { user: req.user });
-});
+router.post('/guarderia/login', passport.authenticate('local2', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+  passReqToCallback: true,
+}));
 
 router.get('/logout', (req, res) => {
   req.logout();
